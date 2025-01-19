745abd49ad885d5090aab364f970c77bf659ebcd
Damir <adidukik@gmail.com>
2025-01-20 00:25:39 +0900

    add footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React from 'react';
+import { FaHeart } from 'react-icons/fa';
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className='flex justify-center items-center h-10'>
+      <p className='flex items-center gap-1'>
+        Made with <FaHeart className='text-red-500' /> in Seoul
+      </p>
+    </div>
+  );
 }
 
 export default Footer;
